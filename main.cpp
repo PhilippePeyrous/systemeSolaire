7f601b59f275d83b8ae9706461eed20c3a6907b2
Philippe <philippe.peyrous@gmail.com>
2015-10-22 23:41:37 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Test C++14" << endl;
+
+    int d = 0b00101010;           // Base binaire (2)
+
+    std::cout << d << std::endl;  // affiche 42
+
+     cout << "succes" << endl;
+
+    return 0;
+}
